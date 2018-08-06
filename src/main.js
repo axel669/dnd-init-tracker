a8438df9786ea46a56b636a1f744fad4aa9ca7ed
axel669 <haryuu.hanekata@gmail.com>
2018-08-05 20:18:38 -0700

    simplified more

--- src/main.js
+++ src/main.js
@@ -15,8 +15,8 @@ import update from 'immutable-update-values';
 // window.Router = Router;
 // window.actions = actions;
 
-// class MainMenu extends React.Component {
-// }
+const cblog = ::console.log;
+
 const MainMenu = ({toggle}) => {
     const route = url =>
         () => {
@@ -45,11 +45,26 @@ const storage = {
     }
 };
 
+const bton = b => b ? 1 : 0;
+const ston = s => parseInt(s);
+
 class AddPlayer extends doric.baseComponent {
     constructor(props) {
         super(props);
         this.state = {name: "", init: "", player: true, id: Date.now()};
         this.link = this.createLinks(...Object.keys(this.state));
+    }
+
+    @autobind
+    cancel() {
+        this.props.close(null);
+    }
+    @autobind
+    add() {
+        const c = {...this.state};
+        c.init = ston(c.init);
+        c.playerValue = bton(c.player);
+        this.props.close(c);
     }
 
     render() {
@@ -61,19 +76,15 @@ class AddPlayer extends doric.baseComponent {
             <React.Fragment>
                 <doric.input.text label="Name" value={name} onChange={this.link.name} />
                 <doric.input.number label="Initiative" value={init} onChange={this.link.init} />
-                <doric.radio value={player} onChange={this.link.player}>
-                    <option value={true} label="Player" />
-                    <option value={false} label="Enemy" />
-                </doric.radio>
             </React.Fragment>
         );
         const actions = (
             <doric.grid>
                 <doric.grid.col size={6}>
-                    <doric.button block danger flat text="Cancel" onTap={() => close(null)} />
+                    <doric.button block danger flat text="Cancel" onTap={this.cancel} />
                 </doric.grid.col>
                 <doric.grid.col size={6}>
-                    <doric.button block primary flat text="Add" onTap={() => close(this.state)} disabled={disabled} />
+                    <doric.button block primary flat text="Add" onTap={this.add} disabled={disabled} />
                 </doric.grid.col>
             </doric.grid>
         );
@@ -81,52 +92,109 @@ class AddPlayer extends doric.baseComponent {
     }
 }
 
-const Creature = ({creature, moveUp, moveDown, remove}) => {
-    // return <div>{creature.name}</div>;
+doric.style.add({
+    'center-text': {
+        display: 'table',
+        width: '100%',
+        height: '100%'
+    },
+    "center-text::after": {
+        display: 'table-cell',
+        textAlign: 'center',
+        verticalAlign: 'middle',
+        content: 'attr(data-text)'
+    },
+    "doric-button[small]": {
+        padding: 1
+    },
+    "doric-card[selected='true']": {
+        backgroundColor: '#a2d3e2'
+    }
+});
+const Creature = ({creature, onFocus, selected}) => {
+    const focus = () => onFocus(creature.id);
+
     return (
-        <doric.card>
-            {/* <doric.card.title main={creature.name} subtitle={creature.init} /> */}
-            <doric.grid>
-                <doric.grid.col size={2}>
-                    {creature.init}
-                </doric.grid.col>
-                <doric.grid.col size={10}>
-                    {creature.name}
-                </doric.grid.col>
-
-                <doric.grid.break />
-
-                <doric.grid.col size={4}>
-                    <doric.button flush block>
-                        <doric.icon icon="ion-trash-b" />
-                    </doric.button>
-                </doric.grid.col>
-                <doric.grid.col size={4}>
-                    <doric.button flush block>
-                        <doric.icon icon="ion-chevron-down" />
-                    </doric.button>
-                </doric.grid.col>
-                <doric.grid.col size={4}>
-                    <doric.button flush block>
-                        <doric.icon icon="ion-chevron-up" />
-                    </doric.button>
-                </doric.grid.col>
-            </doric.grid>
+        <doric.card tabIndex={1} onFocus={focus} selected={selected}>
+            {creature.init} {creature.name}
         </doric.card>
     );
 };
 
+doric.style.add({
+    "side-menu": {
+        position: 'absolute',
+        right: '100%',
+        bottom: 0,
+        width: 100,
+    },
+    "action-menu": {
+        position: 'fixed',
+        bottom: 0,
+        right: 0
+    },
+    "doric-button[circle]": {
+        width: 40,
+        height: 40
+    }
+});
+
+const sortFunc = (a, b) => {
+    const initCheck = a.init - b.init;
+    if (initCheck !== 0) {
+        return initCheck;
+    }
+    return a.playerValue - b.playerValue;
+}
 class InitTracker extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {inits: storage.get('inits', [])};
+        this.state = {
+            inits: storage.get('inits', []),
+            selected: null,
+            // menuOpen: false
+        };
     }
 
     @autobind
-    moveUp(index) {
+    moveUp() {
     }
     @autobind
-    moveDown(index) {
+    moveDown() {
+        const {inits: oldInits, selected} = this.state;
+
+        if (oldInits.length < 2) {
+            return;
+        }
+
+        const item = oldInits.find(i => i.id === selected);
+        const index = oldInits.indexOf(item);
+
+        if (index === oldInits.length - 1) {
+            return;
+        }
+
+        const inits = [...oldInits];
+        inits.splice(index, 1);
+        inits.splice(index + 1, 0, item);
+        this.setState({inits});
+    }
+    @autobind
+    async edit() {
+    }
+    @autobind
+    async remove() {
+        const selected = this.state.selected;
+        const name = this.state.inits.find(i => i.id === selected).name;
+        const confirm = await this.dialog.confirm(
+            `Remove ${name} from initiative?`,
+            "Remove"
+        );
+
+        if (confirm === true) {
+            const inits = this.state.inits.filter(i => i.id !== selected);
+            this.setState({inits, selected: null});
+        }
     }
 
     @autobind
@@ -144,9 +212,13 @@ class InitTracker extends React.Component {
         if (creature !== null) {
             let {inits} = this.state;
             inits = update(inits, {$push: creature});
-            // storage.set('inits', inits);
             this.setState({inits});
         }
+    }
+
+    @autobind
+    select(selected) {
+        this.setState({selected});
     }
 
     componentDidUpdate() {
@@ -154,24 +226,30 @@ class InitTracker extends React.Component {
     }
 
     render() {
+        const {inits, selected, menuOpen} = this.state;
+
         return (
             <doric.appContainer title="Init Tracker">
-                <div style={{paddingBottom: 30}}>
-                    {this.state.inits.map(
-                        (creature, index) => <Creature key={creature.id} {...{creature, index}} />
-                    )}
-                </div>
+                {inits.map(
+                    (creature, index) =>
+                        <Creature key={creature.id}
+                            onFocus={this.select}
+                            selected={selected === creature.id}
+                            creature={creature} />
+                )}
+                <div style={{height: 96}} />
 
-                <div style={{position: 'fixed', bottom: 0, right: 0, left: 0}}>
-                    <doric.grid>
-                        <doric.grid.col size={6}>
-                            <doric.button block danger text="Reset" onTap={this.reset} />
-                        </doric.grid.col>
-                        <doric.grid.col size={6}>
-                            <doric.button block primary text="Add" onTap={this.add} />
-                        </doric.grid.col>
-                    </doric.grid>
-                </div>
+                <action-menu>
+                    <doric.iconButton danger action circle icon="ion-refresh" onTap={this.reset} />
+                    <br />
+                    <doric.iconButton primary action circle icon="ion-plus" onTap={this.add} />
+                    <side-menu>
+                        <doric.iconButton disabled={selected === null} action circle icon="ion-chevron-up" onTap={this.moveUp} />
+                        <doric.iconButton disabled={selected === null} danger action circle icon="ion-trash-b" onTap={this.remove} />
+                        <doric.iconButton disabled={selected === null} action circle icon="ion-chevron-down" onTap={this.moveDown} />
+                        <doric.iconButton disabled={selected === null} primary action circle icon="ion-edit" onTap={this.edit} />
+                    </side-menu>
+                </action-menu>
             </doric.appContainer>
         );
     }
@@ -180,17 +258,5 @@ const Main = doric.dialogify(InitTracker);
 
 doric.init(
     <Main />,
-    // <Provider store={store}>
-    //     {/* <Router enforce> */}
-    //         {/* <RouteSwitch>
-    //             <Route path="/" component={PartyManager} />
-    //             <Route path="/inits/:party" component={InitManager} />
-    //         </RouteSwitch> */}
-    //         {/* <RouteSwitch container={doric.appContainer} menu={MainMenu} eval-title={t => t.title}>
-    //             <Route path="/" component={PartyManager} title="Party Management" />
-    //             <Route path="/" component={InitManager} title="Initiatives" />
-    //         </RouteSwitch> */}
-    //     {/* </Router> */}
-    // {/* </Provider>, */}
     document.querySelector("app-root")
 );
